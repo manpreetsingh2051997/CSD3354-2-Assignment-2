aebb1432609a2929cda0a312edfe20a428238ab7
visualstudio <visualstudio@122-PC32>
2019-03-08 18:45:57 -0500

    lab_2

--- C0726407,(C0725642),assignment2/Program.cs
+++ C0726407,(C0725642),assignment2/Program.cs
@@ -4,34 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1 { 
-namespace lab
+namespace C0726407__C0725642__assignment2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DelegateExercises delegateExercises = new DelegateExercises();
+
+            delegateExercises.Method2();
         }
     }
 
+
     public class DelegateExercises
     {
-        // Student ID:Manpreet singh sidhu Singh Sidhu Student ID:C0726407
-        //Student ID:Ashwinder Singh Student ID:C0725642
-        // Assignment 2
-        //March 7, 2019
         public delegate void MyDelegate();
         void Method1()
         {
             Console.WriteLine("Method1");
             Console.ReadLine();
         }
-            public void Method2()
-            {
-                MyDelegate myDelegate = new MyDelegate(Method1);
-                myDelegate();
-            }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
 }
