65119fe785d8dea34c58318a814dbfbb5ec6ce85
visualstudio <visualstudio@122-PC32>
2019-03-08 17:51:19 -0500

    Add project files.

---
+++ C0726407,(C0725642),assignment2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0726407__C0725642__assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
