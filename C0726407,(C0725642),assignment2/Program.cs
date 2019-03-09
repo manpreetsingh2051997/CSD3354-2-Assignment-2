fff770806491a05fdaf60bc6f76ca0684f11e516
visualstudio <visualstudio@122-PC32>
2019-03-08 20:39:10 -0500

    lab-10

--- C0726407,(C0725642),assignment2/Program.cs
+++ C0726407,(C0725642),assignment2/Program.cs
@@ -5,12 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace C0726407__C0725642__assignment2
-//Manpreet Singh Sidhu C0730484
+//Manpreet Singh Sidhu C0726407
 //Ashwinder Singh C0725642
 //Assigmnment 2 
 //March 8,2019
 //Csd3354-Assignment2
 {
+
     class Program
     {
         static void Main(string[] args)
@@ -45,6 +46,7 @@ namespace C0726407__C0725642__assignment2
 
             MyDelegate myDelegate = new MyDelegate(Method1);
             myDelegate();
+            System.Console.WriteLine(myDelegate.ToString());
 
 
         }
@@ -54,6 +56,3 @@ namespace C0726407__C0725642__assignment2
 
     }
 }
-
-
-
