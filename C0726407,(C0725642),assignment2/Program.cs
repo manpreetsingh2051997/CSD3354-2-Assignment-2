1f179822342a9933813a6dadb8a7add14ccc028a
visualstudio <visualstudio@122-PC32>
2019-03-08 20:43:28 -0500

    Lab-11

--- C0726407,(C0725642),assignment2/Program.cs
+++ C0726407,(C0725642),assignment2/Program.cs
@@ -22,37 +22,27 @@ namespace C0726407__C0725642__assignment2
 
 
         }
+        public delegate void MyDelegate();
 
-
-    }
-    public delegate void MyDelegate();
-    public class DelegateExercises
-
-    {
-
-        void Method1()
-
+        public class DelegateExercises
         {
 
-            System.Console.WriteLine("Method1");
-
-        }
 
 
 
-        public void Method3()
+            void Method3()
 
-        {
+            {
 
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-            System.Console.WriteLine(myDelegate.ToString());
+
+                System.Console.WriteLine(MyDelegate.ToString());
+
+
+            }
+
+
 
 
         }
-
-
-
-
     }
 }
