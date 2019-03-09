74bfae236d87340cbfb00f9472fde7a105a8b339
visualstudio <visualstudio@122-PC32>
2019-03-08 19:59:05 -0500

    Lab-8

--- C0726407,(C0725642),assignment2/Program.cs
+++ C0726407,(C0725642),assignment2/Program.cs
@@ -16,42 +16,25 @@ namespace C0726407__C0725642__assignment2
         static void Main(string[] args)
         {
             DelegateExercises delegateExercises = new DelegateExercises();
-
             delegateExercises.Method3();
+            Console.ReadLine();
 
 
         }
 
 
     }
+    public delegate void MyDelegate();
     public class DelegateExercises
 
     {
-        public delegate int MyDelegate(int intValue);
 
-
-
-        public int Method1(int intMethod1)
+        void Method1()
 
         {
 
-            return intMethod1 * 4;
+            System.Console.WriteLine("Method1");
 
-        }
-
-
-
-        public int Method2(int intMethod2)
-
-        {
-
-            return intMethod2 * 20;
-
-        }
-        public void Method4(MyDelegate myDelegate)
-        {
-            for (int i = 1; i <= 5; i++)
-                System.Console.WriteLine(myDelegate(i) + " ");
         }
 
 
@@ -61,10 +44,16 @@ namespace C0726407__C0725642__assignment2
         {
 
             MyDelegate myDelegate = new MyDelegate(Method1);
-            Method4(myDelegate);
-            myDelegate = new MyDelegate(Method2);
-            Method4(myDelegate);
+            myDelegate();
+
 
         }
+
+
+
+
     }
 }
+
+
+
